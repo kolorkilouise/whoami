370a3e7b4ce00f91531557e5bf18d54d453df0ab
Vinicius Nunes Martins <nunesmartinsvinicius@gmail.com>
2026-01-27 14:52:07 -0300

    refactor(language-selector): migrate to tailwindcss v4 and add to header

--- src/app/pages/whoami/components/footer/footer.component.ts
+++ src/app/pages/whoami/components/footer/footer.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [],
   templateUrl: './footer.component.html',
-  host: { class: 'block' }
+  host: { class: 'block' },
 })
 export class FooterComponent {
-
+  year = new Date().getFullYear();
 }
